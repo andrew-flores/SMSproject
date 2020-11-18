fcbfd1f47e1c1997799581062177a877a186e3ce
trank <trank@DESKTOP-MCRP9DH>
2020-11-18 14:58:39 -0800

    Updated Student object, will make sense later

--- SMS-project/src/Student.java
+++ SMS-project/src/Student.java
@@ -1,11 +1,31 @@
 
 public class Student {
-	/*
-	 * Nam made these, delete this comment later pls
-	 */
+	
 	private String first;
 	private String last;
 	private double GPA;
 	private String username;
+	private String password;
+	private double totalBalance;
 	
+	public Student(String first, String last) {
+		this.first = first;
+		this.last = last;
+	}
+	
+	public String getFirst() {
+		return this.first;
+	}
+	
+	public String getLast() {
+		return this.last;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
 }
