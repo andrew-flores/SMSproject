72ed7943e6dd6ecb8c0a6077f60e90713a8bf440
trankhainam97 <61358799+trankhainam97@users.noreply.github.com>
2020-11-25 16:41:16 -0800

    Add files via upload

---
+++ Sms.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+/**
+ * 
+ * @author trank
+ *
+ */
+public class Sms {
+
+	/*
+	 * Nam made this, delete later
+	 */
+	private static final int MAX_ATTEMPT = 5;
+
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		try {
+			if (Authenticate.run(s) == MAX_ATTEMPT)
+				throw new Exception();
+
+			Student student = new Student(Authenticate.getIndex());
+
+			// menu goes here
+			System.out.println("Welcome, " + student.getFirst());
+			menu(s);
+			System.out.println("Goodbye, " + student.getFirst());
+		} catch (Exception e) {
+			// I'm bored do whatever here I guess idc sry if it lags your system
+			System.out.println("YOU FELL FOR IT, FOOL!\nTHUNDER CROSS SPLIT ATTACK!");
+		}
+	}
+	
+	/**
+	 * This method runs the menu option in a loop until the user wants to log off
+	 * @param s Scanner for user input
+	 */
+	public static void menu(Scanner s) {
+		boolean exit = false;
+		while (!exit) {
+			System.out.println("---------------------------------------------");
+			System.out.println("Please select a menu option:");
+			System.out.println("1) Change password");
+			System.out.println("2) View courses");
+			System.out.println("3) Add courses");
+			System.out.println("4) Account inquiry");
+			System.out.println("q) Logout");
+			System.out.print("> ");
+			
+			String menu_input = s.next();
+			switch (menu_input) {		// method calls down here
+			case "1": 
+				break;
+			case "2":
+				break;
+			case "3":
+				break;
+			case "4":
+				break;
+			case "q":
+				System.out.println("Are you sure you want to log out? (YES/NO) ");	
+				String confirm = s.next();
+				if (confirm.equalsIgnoreCase("YES")) {
+					exit = true;	// raise flag
+					System.out.println("Logging you out now...");
+				}
+				break;
+			default: System.out.println("ERROR! Please enter a correct option.");	
+			}
+		}
+	}
+}
