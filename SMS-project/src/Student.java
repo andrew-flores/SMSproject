f8468487a3043f56bf2fe88dca5dc41dd203176b
trank <trank@DESKTOP-MCRP9DH>
2020-11-28 13:18:12 -0800

    Minor update

--- SMS-project/src/Student.java
+++ SMS-project/src/Student.java
@@ -11,17 +11,14 @@ import java.util.Scanner;
 public class Student {
 	private String first;
 	private String last;	
-	private String username;	
-	private String password;
+	private String username;
 	private String GPA;
-	private double totalBalance;
-	
+	private File file1 = new File("names.txt");
+	private File file2 = new File("crdntl.txt");
 	public Student(int index) {
 		if (index < 0)
 			throw new IllegalArgumentException();
 		
-		File file1 = new File("names.txt");
-		File file2 = new File("crdntl.txt");
 		Scanner s;
 		
 		try {
@@ -38,16 +35,13 @@ public class Student {
 				
 				i++;
 			}
-			
-			// auto generate username 
-			// write username to an external file (.csv or .txt)
-			
+		
 			i = 0;
 			s = new Scanner(file2).useDelimiter(",|\\s");
 			while (s.hasNextLine()) {
 				if (i == index) {
 					this.username = s.next();
-					this.password = s.next();
+					s.next();
 					this.GPA = s.next();
 					break;
 				} else
